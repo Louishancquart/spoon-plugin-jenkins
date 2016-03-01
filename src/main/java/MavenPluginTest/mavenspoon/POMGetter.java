3ead4459f3ad14951d1a841b57a91b69d1da1d98
Louishancquart <hancquartlouis@gmail.com>
2016-03-01 01:46:55 +0100

    pom analyzer works. Added POMGetter class
    add info : version, module artifactId, plugins Check
    missing  : Git commit identifier
    
    gather step 2 info
    
    created infoGetter , writes file to store info in target
    
    step 2
    
    step 2 finished

---
+++ src/main/java/MavenPluginTest/mavenspoon/POMGetter.java
@@ -0,0 +1,232 @@
+package MavenPluginTest.mavenspoon;
+
+import hudson.FilePath;
+import hudson.remoting.VirtualChannel;
+import org.jenkinsci.remoting.RoleChecker;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import java.io.File;
+import java.io.IOException;
+
+
+/**
+ * Created by Louis on 01.03.16.
+ */
+public class POMGetter {
+
+    public final FilePath workspace;
+    private static final String BUILD_FILE = "pom.xml";
+
+    public POMGetter(FilePath workspace) {
+        this.workspace = workspace;
+    }
+
+    public String getInfo(String expression_to_compile) {
+
+
+        String info = null;
+
+        Document document = null;
+        try {
+            document = getPom(workspace);
+        } catch (InvalidBuildFileFormatException | IOException e) {
+            e.printStackTrace();
+        }
+
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        XPathExpression expression;
+        try {
+            expression = xPath.compile("/project/"+expression_to_compile);
+            info = expression.evaluate(document);
+
+        } catch (XPathExpressionException e) {
+            try {
+                assert document != null;
+                throw new InvalidBuildFileFormatException(document.getBaseURI()
+                        + " is not a valid POM file.");
+            } catch (InvalidBuildFileFormatException e1) {
+                e1.printStackTrace();
+            }
+
+
+        }
+
+        if (info == null || info.length() == 0) {
+            try {
+                assert document != null;
+                throw new InvalidBuildFileFormatException(
+                        "No info information found in " + document.getBaseURI());
+            } catch (InvalidBuildFileFormatException e) {
+                e.printStackTrace();
+            }
+        }
+        return info;
+    }
+
+
+    public Boolean hasPlugin(String pluginArtifactId) {
+
+        String info = null;
+        Document document = null;
+
+        try {
+            document = getPom(workspace);
+        } catch (InvalidBuildFileFormatException | IOException e) {
+            e.printStackTrace();
+        }
+
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        XPathExpression expression;
+        try {
+            expression = xPath.compile("/project/build/plugins/plugin[artifactId='pluginArtifactId']");
+            info = expression.evaluate(document);
+
+        } catch (XPathExpressionException e) {
+            try {
+                assert document != null;
+                throw new InvalidBuildFileFormatException(document.getBaseURI()
+                        + " is not a valid POM file.");
+            } catch (InvalidBuildFileFormatException e1) {
+                e1.printStackTrace();
+            }
+
+
+        }
+
+        if (info == null || info.length() == 0) {
+            try {
+                assert document != null;
+                throw new InvalidBuildFileFormatException(
+                        "No info information found in " + document.getBaseURI());
+            } catch (InvalidBuildFileFormatException e) {
+                e.printStackTrace();
+            }
+        }
+        return info.contains(pluginArtifactId);
+    }
+
+    public Boolean hasNode(String node) {
+
+        String info = null;
+        Document document = null;
+
+        try {
+            document = getPom(workspace);
+        } catch (InvalidBuildFileFormatException | IOException e) {
+            e.printStackTrace();
+        }
+
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        XPathExpression expression;
+        try {
+            expression = xPath.compile("/project/"+node+"/");
+            info = expression.evaluate(document);
+
+        } catch (XPathExpressionException e) {
+            try {
+                assert document != null;
+                throw new InvalidBuildFileFormatException(document.getBaseURI()
+                        + " is not a valid POM file.");
+            } catch (InvalidBuildFileFormatException e1) {
+                e1.printStackTrace();
+            }
+
+        }
+
+        return info == null || info.length() == 0;
+    }
+
+
+
+    public String getJavaVersion(String pluginArtifactId) {
+
+        String info = null;
+        Document document = null;
+
+        try {
+            document = getPom(workspace);
+        } catch (InvalidBuildFileFormatException | IOException e) {
+            e.printStackTrace();
+        }
+
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        XPathExpression expression;
+        try {
+            expression = xPath.compile("/project/build/plugins/plugin[artifactId='pluginArtifactId']/configuration/version");
+            info = expression.evaluate(document);
+
+        } catch (XPathExpressionException e) {
+            try {
+                assert document != null;
+                throw new InvalidBuildFileFormatException(document.getBaseURI()
+                        + " is not a valid POM file.");
+            } catch (InvalidBuildFileFormatException e1) {
+                e1.printStackTrace();
+            }
+
+
+        }
+
+        if (info == null || info.length() == 0) {
+            try {
+                assert document != null;
+                throw new InvalidBuildFileFormatException(
+                        "No info information found in " + document.getBaseURI());
+            } catch (InvalidBuildFileFormatException e) {
+                e.printStackTrace();
+            }
+        }
+        return info;
+    }
+
+    public Document getPom(FilePath workspace)
+            throws InvalidBuildFileFormatException, IOException {
+
+        FilePath pom;
+
+        pom = new FilePath(workspace, BUILD_FILE);
+
+        Document pomDocument;
+        try {
+            pomDocument = pom.act(new FilePath.FileCallable<Document>() {
+                public Document invoke(File pom, VirtualChannel channel)
+                        throws IOException, InterruptedException {
+
+                    try {
+                        DocumentBuilder documentBuilder;
+                        documentBuilder = DocumentBuilderFactory.newInstance()
+                                .newDocumentBuilder();
+                        return documentBuilder.parse(pom);
+
+                    } catch (SAXException | ParserConfigurationException e) {
+                        throw new InterruptedException(pom
+                                .getAbsolutePath()
+                                + " is not a valid POM file.");
+                    }
+                }
+
+                public void checkRoles(RoleChecker arg0) throws SecurityException {
+                }
+
+            });
+        } catch (InterruptedException e) {
+            throw new InvalidBuildFileFormatException(e.getMessage());
+        }
+
+        return pomDocument;
+    }
+
+    public static class InvalidBuildFileFormatException extends Exception {
+        public InvalidBuildFileFormatException(String message) {
+            super(message);
+        }
+    }
+}
---
+++ src/main/java/MavenPluginTest/mavenspoon/ProjectInfoGetter.java
@@ -0,0 +1,121 @@
+package MavenPluginTest.mavenspoon;
+
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.Launcher;
+import hudson.model.AbstractProject;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.BuildStepMonitor;
+import hudson.tasks.Builder;
+import jenkins.tasks.SimpleBuildStep;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+
+/**
+ * Sample {@link Builder}.
+ * <p>
+ * <p>
+ * When the user configures the project and enables this builder,
+ * {@link DescriptorImpl#newInstance(StaplerRequest)} is invoked
+ * and a new {@link ProjectInfoGetter} is created. The created
+ * instance is persisted to the project configuration XML by using
+ * XStream, so this allows you to use instance fields
+ * to remember the configuration.
+ * <p>
+ * <p>
+ * When a build is performed, the {@link #perform} method will be invoked.
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public class ProjectInfoGetter extends Builder implements SimpleBuildStep {
+
+    // private final String name;
+
+    // Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
+    @DataBoundConstructor
+    public ProjectInfoGetter() {
+//        this.name = name;
+
+    }
+
+
+//
+//    // Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
+//    @DataBoundConstructor
+//    public POM_analyzer() {
+//    }
+
+    @Override
+    public BuildStepMonitor getRequiredMonitorService() {
+        return BuildStepMonitor.BUILD;
+    }
+
+
+    /*
+
+
+        /**
+        * We'll use this from the <tt>config.jelly</tt>.
+        * /
+        public String getName() {
+        return name;
+        }
+
+        */
+    @Override
+    public void perform(Run<?, ?> build, FilePath workspace, Launcher launcher, TaskListener listener) throws IOException {
+
+        InfoGetter infos = new InfoGetter(new POMGetter(workspace), listener, build);
+        String[] modules = infos.getInfos();
+        infos.writeToFile(modules);
+
+        //insert spoon-plugin in the pom of the project
+//        POMModifier pm = new POMModifier(new POMGetter(workspace), listener, build);
+
+        //take the time
+        // run the project
+        // take the time
+        //publish infos
+    }
+
+
+    /**
+     * Descriptor for {@link ProjectInfoGetter}. Used as a singleton.
+     * The class is marked as public so that it can be accessed from views.
+     * <p>
+     * <p>
+     * See <tt>src/main/resources/hudson/plugins/hello_world/ProjectInfoGetter/*.jelly</tt>
+     * for the actual HTML fragment for the configuration screen.
+     */
+    @Extension // This indicates to Jenkins that this is an implementation of an extension point.
+    public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+
+        /**
+         * In order to load the persisted global configuration, you have to
+         * call load() in the constructor.
+         */
+        public DescriptorImpl() {
+            load();
+        }
+
+        public boolean isApplicable(Class<? extends AbstractProject> aClass) {
+            return true;
+        }
+
+        /**
+         * This human readable name is used in the configuration screen.
+         */
+        public String getDisplayName() {
+            return "Analyse POM file";
+        }
+
+    }
+}
+
