0d3ae9ed7c3f4adadd958150ac92f4b008394416
Louishancquart <hancquartlouis@gmail.com>
2016-04-17 21:47:16 +0200

    debugging - not working commit /!\

--- src/main/java/MavenPluginTest/mavenspoon/POMModifier.java
+++ src/main/java/MavenPluginTest/mavenspoon/POMModifier.java
@@ -1,16 +1,15 @@
 package MavenPluginTest.mavenspoon;
 
 import hudson.FilePath;
-import hudson.model.Run;
 import hudson.model.TaskListener;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Text;
+import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.*;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.*;
@@ -24,11 +23,13 @@ public class POMModifier {
     private final FilePath workspace;
     private Element p;
     private NodeList nodes;
+//    private Node newNode;
 
     public POMModifier(POMGetter pomGetter, TaskListener listener, FilePath worspace) {
         this.pom = pomGetter;
         this.listener = listener;
         this.workspace = worspace;
+
     }
 
 
@@ -42,34 +43,62 @@ public class POMModifier {
 
         Document doc = pom.getPom(this.workspace);
 
-        //create plugin nodes if doesn't exist
-        if( !pom.hasNode("build"))
-        {
-          p = doc.createElement("build");
-            doc.getParentNode().insertBefore(p,doc.getParentNode());
-        }
+        //debug
+        p = doc.createElement("build");
+        nodes = doc.getElementsByTagName("project");
 
-        if(!pom.hasNode("plugins"))
-        {
-            p = doc.createElement("plugins");
-            nodes = doc.getElementsByTagName("build");
-            nodes.item(0).getParentNode().insertBefore(p, nodes.item(0));
-        }
+        //        //debug
+        listener.getLogger().println("p "+ p.toString());
+        listener.getLogger().println("node "+nodes.toString());
+        listener.getLogger().println("parent "+nodes.item(0).toString());
+
+        nodes.item(0).getParentNode().insertBefore(p, nodes.item(0));
+        p = doc.createElement("plugins");
+
+        nodes = doc.getElementsByTagName("build");
+        nodes.item(0).getParentNode().insertBefore(p, nodes.item(0).getFirstChild());
+
+
+//        //create plugin nodes if doesn't exist
+//        if (!pom.hasNode("build")) {
+//            p = doc.createElement("build");
+//            nodes = doc.getElementsByTagName("project");
+//            //debug
+//            listener.getLogger().println("node1 "+nodes.toString());
+//            listener.getLogger().println("parent1 "+nodes.item(0).toString());
+//
+//            nodes.item(0).getParentNode().insertBefore(p, nodes.item(0).getFirstChild());
+//        }
+//
+//        if (!pom.hasNode("plugins")) {
+//            p = doc.createElement("plugins");
+//            nodes = doc.getElementsByTagName("build");
+//
+//            //debug
+//            listener.getLogger().println("node2 "+nodes.toString());
+//            listener.getLogger().println("parent2 "+nodes.item(0).toString());
+//
+//            nodes.item(0).getParentNode().insertBefore(p, nodes.item(0).getFirstChild());
+//        }
 
 //      Add spoon plugin in the DOM
 
 //        <plugin>
         p = doc.createElement("plugin");
         nodes = doc.getElementsByTagName("plugins");
-        nodes.item(0).getParentNode().insertBefore(p, nodes.item(0));
 
+//        //debug
+//        listener.getLogger().println("node "+nodes.toString());
+//        listener.getLogger().println("parent "+nodes.item(0).toString());
+
+        nodes.item(0).getParentNode().insertBefore(p, nodes.item(0).getFirstChild());
         nodes = doc.getElementsByTagName("plugin");
 
 //        <groupId>fr.inria.gforge.spoon</groupId>
         p = doc.createElement("groupId");
         Text innerXML = doc.createTextNode("fr.inria.gforge.spoon");
         p.appendChild(innerXML);
-        nodes.item(0).getParentNode().insertBefore(p, nodes.item(0));
+        nodes.item(0).getParentNode().insertBefore(p, nodes.item(0).getFirstChild());
 
 //        <artifactId>spoon-maven-plugin</artifactId>
         p = doc.createElement("artifactId");
@@ -85,19 +114,19 @@ public class POMModifier {
 
 //        <executions>
         p = doc.createElement("executions");
-        nodes.item(0).getParentNode().insertBefore(p, nodes.item(0));
+        nodes.item(0).getParentNode().insertBefore(p, nodes.item(0).getFirstChild());
 
 //        <execution>
         nodes = doc.getElementsByTagName("executions");
         p = doc.createElement("execution");
-        nodes.item(0).getParentNode().insertBefore(p, nodes.item(0));
+        nodes.item(0).getParentNode().insertBefore(p, nodes.item(0).getFirstChild());
 
 //        <phase>generate-sources</phase>
         nodes = doc.getElementsByTagName("execution");
         p = doc.createElement("phase");
         innerXML = doc.createTextNode("generate-sources");
         p.appendChild(innerXML);
-        nodes.item(0).getParentNode().insertBefore(p, nodes.item(0));
+        nodes.item(0).getParentNode().insertBefore(p, nodes.item(0).getFirstChild());
 
 //        <goals>
         p = doc.createElement("goals");
@@ -108,7 +137,7 @@ public class POMModifier {
         p = doc.createElement("goal");
         innerXML = doc.createTextNode("generate");
         p.appendChild(innerXML);
-        nodes.item(0).getParentNode().insertBefore(p, nodes.item(0));
+        nodes.item(0).getParentNode().insertBefore(p, nodes.item(0).getFirstChild());
 
 
 //      Get the XML to string
@@ -121,12 +150,12 @@ public class POMModifier {
         transformer.transform(source, result);
 
         String xmlOutput = result.getWriter().toString();
-        System.out.println(xmlOutput);
+        listener.getLogger().println(xmlOutput);
 
 
 //      Write XML in the file
 
-
+        writeToFile(xmlOutput);
 
         return true;
     }
@@ -141,25 +170,25 @@ public class POMModifier {
             FilePath pomfile = new FilePath(workspace, "pom.xml");
 
             File file = new File(String.valueOf(pomfile)); // A tester ..
-            if(!file.delete()){
+            if (!file.delete()) {
                 listener.getLogger().println("new pom not created");
-            }else if (!file.createNewFile()) {
+            } else if (!file.createNewFile()) {
                 listener.getLogger().println("new pom not created");
             }
 
 
-        sw = new StringWriter();
-        bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "UTF-8"));
-        StringBuffer sb = sw.getBuffer();
-        bw.write(sb.toString());
-        bw.flush();
+            sw = new StringWriter();
+            bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "UTF-8"));
+            StringBuffer sb = sw.getBuffer();
+            bw.write(sb.toString());
+            bw.flush();
 
 
-        listener.getLogger().println(sb);
+            listener.getLogger().println(sb);
 
 
-    } catch (IOException e) {
-        e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         } finally {
             // releases any system resources associated with the stream
             if (sw != null)
